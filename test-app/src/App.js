726eef61306a87ec2790feea4f744f8eb3bd9750
vijay8077 <vijays.aussizz@gmail.com>
2020-03-06 15:17:34 +0530

    new name

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Test Rakshit</p>
+        <p>Hello Vijay</p>
         <a
           className="App-link"
           href="https://reactjs.org"
