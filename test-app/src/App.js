25783ec41dc1c51cc7bbe92b98c7bc20849f0529
rakshitpatel123 <rakshit.aussizz@gmail.com>
2020-03-06 15:21:07 +0530

    Greeting

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vijay</p>
+        <p>Hello Vijay su kare che</p>
         <a
           className="App-link"
           href="https://reactjs.org"
