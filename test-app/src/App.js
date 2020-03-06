deb863062bb2c8dce4d3950c4f074e2ce7b908c2
rakshitpatel123 <rakshit.aussizz@gmail.com>
2020-03-06 12:45:20 +0530

    change name

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -1,15 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Test Vijay
-        </p>
+        <p>Test Rakshit</p>
         <a
           className="App-link"
           href="https://reactjs.org"
