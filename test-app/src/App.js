81536c0a48d17f04a9dcf130dc328d9afe2c3f07
vijay8077 <vijays.aussizz@gmail.com>
2020-03-06 15:24:12 +0530

    final testing

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vijay su kare che</p>
+        <p>Wel Come React Git</p>
         <a
           className="App-link"
           href="https://reactjs.org"
